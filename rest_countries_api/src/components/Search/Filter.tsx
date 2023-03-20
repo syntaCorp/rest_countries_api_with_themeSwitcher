7e62f4c04d027233e24b07c9329ba4f05ea0da80
cyberstrong <108492938+syntaCorp@users.noreply.github.com>
2023-03-20 15:51:24 +0000

    header and search bar

---
+++ rest_countries_api/src/components/Search/Filter.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Filter(): React.ReactElement {
+
+    return (
+        <React.Fragment>
+           <div className="filter__bar">
+                <div>Filter by region</div>
+                <svg width="16px" height="16px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fillRule="evenodd" clipRule="evenodd" d="M4.29289 8.29289C4.68342 7.90237 5.31658 7.90237 5.70711 8.29289L12 14.5858L18.2929 8.29289C18.6834 7.90237 19.3166 7.90237 19.7071 8.29289C20.0976 8.68342 20.0976 9.31658 19.7071 9.70711L12.7071 16.7071C12.3166 17.0976 11.6834 17.0976 11.2929 16.7071L4.29289 9.70711C3.90237 9.31658 3.90237 8.68342 4.29289 8.29289Z" fill="#000000"></path> </g></svg>
+           </div>
+        </ React.Fragment>
+    );
+}
