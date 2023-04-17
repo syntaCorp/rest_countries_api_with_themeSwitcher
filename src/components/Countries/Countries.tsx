0dc96dc7b74acdad2f1ab98872cea08525b14c25
cyberstrong <108492938+syntaCorp@users.noreply.github.com>
2023-04-17 21:49:17 +0000

    added empty search result prompt

--- src/components/Countries/Countries.tsx
+++ src/components/Countries/Countries.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-import { CountryType, searchPropType } from "../../types/types";
+import { CountryType, searchPropType, countryNameType } from "../../types/types";
 import { selectAllCountries, selectErrorMessage, selectStatus } from "../../features/countriesSlice";
 import { fetchCountries } from "../../features/countriesSlice";
 import CountriesItem from "./CountriesItem";
+import { current } from "@reduxjs/toolkit";
 
 
 export default function Countries({ searchTerm, region }: searchPropType): React.ReactElement {
@@ -27,55 +28,64 @@ export default function Countries({ searchTerm, region }: searchPropType): React
     //alphabetically sort countries 
     const sortedCountries = [...countries].sort((item: CountryType, _item: CountryType) => item.name.common.localeCompare(_item.name.common));
 
-    // function filterdCountriesData(countries: CountryType[]) {
-    //     return countries.filter((country: CountryType) => {
-
-    //         if (country.region === region) {
-    //             return searchParam.some((currentParam: string) => {
-    //                 return country[currentParam as keyof country].common.toLowerCase().indexOf(searchTerm) === 0;
-    //             })
-    //         } else if (region === 'Filter by region') {
-    //             return country.region;
-    //         }
-
-    //     })
-    // }
-
-    //existing function to render data
-    function renderData(countries: CountryType[]) {
+    function filterdCountriesData(countries: CountryType[]) {
         return countries.filter((country: CountryType) => {
             if (country.region === region) {
-                return country.name.common.toLowerCase()
-                    .indexOf(searchTerm.toLowerCase()) === 0;
-            }
-            else if ( region === 'Filter by region'){
+                return searchParam.some((currentParam: string) => {
+
+                    // return country[currentParam as keyof CountryType].common.toLowerCase().indexOf(searchTerm) === 0;
+                    if ('common' in country) {
+                        return country[currentParam as keyof CountryType];
+                    }
+                    // toLowerCase().indexOf(searchTerm) === 0;
+                })
+            } else if (region === 'Filter by region') {
                 return country.name.common.toLowerCase().indexOf(searchTerm.toLowerCase()) === 0;
             }
         })
     }
+    filterdCountriesData(sortedCountries);
 
+    function renderData(countries: CountryType[]) {
+        return countries.filter((country: CountryType) => {
+            if (country.region === region) {
+                return country.name.common.toLowerCase()
+                    .indexOf(searchTerm.toLowerCase()) === 0; //match country names that start with the search term
+            }
+            //when no region is selected show all regions unless a region is selected
+            //ensures that all searches match the filtered region by country name
+            else if (region === 'Filter by region') {
+                return country.name.common.toLowerCase().indexOf(searchTerm.toLowerCase()) === 0;
+            }
+        })
+    }
+    console.log('data len', renderData(sortedCountries).length === 0);
     return (
         <React.Fragment>
             {loadStatus === 'loading' ?
                 <div className="status_box"><h1 className="loading">Loading...</h1></div>
-                : loadStatus === 'success' ?
-                    renderData(sortedCountries)
-                        .map((country: any, index: number) => {
-                            return (<CountriesItem
-                                key={country.name.common + index}
-                                name={country.name}
-                                population={country.population}
-                                region={country.region}
-                                capital={country.capital}
-                                flags={country.flags}
-                                alt={country.flags.alt}
-                                cioc={country.cioc}
-                                cca3={country.cca3}
-                            />)
-                        })
-                    : <div className="status_box">
-                        <h1 className="loading">{errorMessage}</h1>
+                : loadStatus === 'success'  && renderData(sortedCountries).length === 0 ?
+                    <div className="status_box">
+                        <h1 >No match found 🙄</h1>
                     </div>
+                    : loadStatus === 'success' ?
+                        renderData(sortedCountries)
+                            .map((country: any, index: number) => {
+                                return (<CountriesItem
+                                    key={country.name.common + index}
+                                    name={country.name}
+                                    population={country.population}
+                                    region={country.region}
+                                    capital={country.capital}
+                                    flags={country.flags}
+                                    alt={country.flags.alt}
+                                    cioc={country.cioc}
+                                    cca3={country.cca3}
+                                />)
+                            })
+                        : <div className="status_box">
+                            <h1 className="loading">{errorMessage}</h1>
+                        </div>
             }
         </ React.Fragment>
     );
